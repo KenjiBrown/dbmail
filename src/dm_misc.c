6169c48e7815f7e999e23f65c91ab7299f437b7a
Alan Hicks <ahicks@p-o.co.uk>
2017-01-15 14:29:20 +0000

    Allow for systems that don't use proc

--- src/dm_misc.c
+++ src/dm_misc.c
@@ -106,6 +106,10 @@ int drop_privileges(char *newuser, char *newgroup)
 
 int get_opened_fd_count(void)
 {
+#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__SUNPRO_C)
+	// BSD like systems don't use proc
+	return 0;
+#else
 	DIR* dir = NULL;
 	struct dirent* entry = NULL;
 	char buf[32];
@@ -122,6 +126,7 @@ int get_opened_fd_count(void)
 	closedir(dir);
 
 	return fd_count - 2; /* exclude '.' and '..' entries */
+#endif
 }
 
 void create_unique_id(char *target, uint64_t message_idnr)
